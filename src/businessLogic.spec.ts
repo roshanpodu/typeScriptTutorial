fae1bb21d604629718d760ca1894785cbe119466
roshanpodu <roshan.poduval@leanix.net>
2023-07-13 10:37:49 -0400

    commit test

--- src/businessLogic.spec.ts
+++ src/businessLogic.spec.ts
@@ -1,25 +1,7 @@
-import { Authenticator, GraphQLClient } from 'leanix-js'
-import { fetchApplications, mutateApplications } from './businessLogic'
-
-const host = process.env.LEANIX_HOST
-const apitoken = process.env.LEANIX_APITOKEN
-
-describe('The business logic', () => {
-  const authenticator = new Authenticator({ host, apitoken })
-  const graphql = new GraphQLClient(authenticator)
-  beforeAll(async () => {
-    expect(typeof host).toEqual('string')
-    expect(typeof apitoken).toEqual('string')
-    await authenticator.start()
-  })
-  afterAll(() => {
-    authenticator.stop()
-  })
-
-  it('fetches applications from the inventory', async () => {
-    const applications = await fetchApplications({ graphql })
-    expect(Array.isArray(applications)).toBeTruthy()
-    const updatedApplications = await mutateApplications({ applications, graphql })
-    expect(Array.isArray(updatedApplications)).toBeTruthy()
-  }, 10000)
+import { sum } from './businessLogic'
+it('computes the sum of two numbers', async () => {
+  const a = 1
+  const b = 2
+  const c = sum(a, b)
+  expect(a + b).toEqual(c + 1)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,29 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
 import figlet from 'figlet'
-import { writeFileSync } from 'node:fs'
-import { Authenticator, GraphQLClient } from 'leanix-js'
-import { fetchApplications, mutateApplications } from './businessLogic'
+import { sum } from './businessLogic'
 import { version, description } from '../package.json'
 
 export interface ICliOptions {
-  host: string
-  apitoken: string
-  query?: string
-  output?: string
+  a: number
+  b: number
 }
 
-console.log(figlet.textSync('LeanIX Ticket #73263 Tool'))
+console.log(figlet.textSync('Sum Calculator'))
 
 const program = new Command()
-
-const DEFAULT_OUTPUT_PATH = 'data.json'
 
 program
   .version(version)
   .description(description)
-  .requiredOption(
-    '-h, --host <your_leanix_api_host>',
-    'LeanIX API host, e.g. "app.leanix.net"'
-  )
-  .requiredOption('-t, --apitoken <your_leanix_api_token>', 'LeanIX API token')
-  .option(
-    '-o, --output <json file path>',
-    'The path for the output file',
-    DEFAULT_OUTPUT_PATH
-  )
+  .requiredOption('-a', '--first', 'number to be summed')
+  .requiredOption('-b', '--second', 'second number to be summed')
   .parse(process.argv)
 
-const {
-  host,
-  apitoken,
-  output = DEFAULT_OUTPUT_PATH
-} = program.opts<ICliOptions>()
-
-const authenticator = new Authenticator({ host, apitoken })
-const graphql = new GraphQLClient(authenticator)
-
+const { a, b } = program.opts<ICliOptions>()
+console.log(program.options)
 void (async () => {
-  await authenticator.start()
-  try {
-    const applications = await fetchApplications({ graphql })
-    const updatedApplications = await mutateApplications({ applications, graphql })
-    writeFileSync(output, JSON.stringify(updatedApplications, null, 2))
-    console.log(`Saved data to ${output}`)
-  } finally {
-    authenticator.stop()
-  }
+  console.log('GOT A', a, b)
+  const c = sum(a, b)
+  console.log(`The sum of ${a} + ${b} is ${c}`)
 })()
